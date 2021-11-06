faf5245e6b170ee988173eedc6b545cdd7a391b1
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-11-06 23:55:12 +0530

    add retention policy & targets

--- src/main/java/com/pethumjeewantha/TableCreation.java
+++ src/main/java/com/pethumjeewantha/TableCreation.java
@@ -1,8 +1,13 @@
 package com.pethumjeewantha;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TableCreation {
 
-    public static void main(String[] args) {
+    public static void init(String url, String userName, String password) throws SQLException {
+        Connection connection = DriverManager.getConnection(url, userName, password);
 
     }
 }
